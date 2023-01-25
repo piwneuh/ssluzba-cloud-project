a53f8f2f7877c5a3010bdd707e48bfcb5713a6d3
piwneuh <filip.pinjuh@gmail.com>
2023-01-25 17:13:49 +0100

    professor visualization

--- ftn/professor/urls.py
+++ ftn/professor/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.professor_registration, name='professor-registration'),
+    path('register/', views.professor_registration, name='professor-registration'),
+    path('', views.professor_table, name='professor-table')
 ]
--- ftn/professor/views.py
+++ ftn/professor/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import ProfessorRegistrationForm
+from .models import Professor
 
 # Create your views here.
+def registration_redirect(request):
+    return redirect('professor-registration')
+
+def professor_table(request):
+    professors = Professor.objects.all()
+    return render(request, 'professor_table.html', {'professors': professors})
+
 def professor_registration(request):
     if request.method == 'POST':
         form = ProfessorRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('professor-table')
     else:
         form = ProfessorRegistrationForm()
     return render(request, 'professor_registration.html', {'form':form})

ebe2c3e5c74ff9ff0d697a2c90337d87bea4c5fc
piwneuh <filip.pinjuh@gmail.com>
2023-01-25 18:39:06 +0100

    docker support added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ssluzba-cloud-project
  Student project focusing on dockerizing multiple django applications
+ Filip Pinjuh
+ E254-2022
--- ftn/ftn/settings.py
+++ ftn/ftn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-39$z6xnt1(@%otgvxm$p^0twp3i-r+g-^9t^h#$tn8i7iox#%u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition

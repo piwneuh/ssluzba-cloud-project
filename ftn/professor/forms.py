b8454195c8f166eaad4b7f201603b95f54d1e649
piwneuh <filip.pinjuh@gmail.com>
2023-01-25 15:11:15 +0100

    student and professor registration

--- ftn/professor/forms.py
+++ ftn/professor/forms.py
@@ -5,3 +5,8 @@ class ProfessorRegistrationForm(forms.ModelForm):
     class Meta:
         model = Professor
         fields = ['first_name', 'last_name', 'email']
+    
+    def save(self):
+        data = self.cleaned_data
+        professor = Professor(first_name=data['first_name'], last_name=data['last_name'], email=data['email'])
+        professor.save()
--- ftn/student/forms.py
+++ ftn/student/forms.py
@@ -5,3 +5,8 @@ class StudentRegistrationForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['first_name', 'last_name', 'email']
+
+    def save(self):
+        data = self.cleaned_data
+        professor = Student(first_name=data['first_name'], last_name=data['last_name'], email=data['email'])
+        professor.save()
